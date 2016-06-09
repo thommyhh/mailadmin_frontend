cc930e1f7747c7d854d2f3248df76061b1e5964d
Fynn Becker <info@fynn-becker.de>
2016-06-09 09:58:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,165 @@
+# Front-End Styleguide
+Front-end development styleguide with Sass, JavaScript and Handlebars.
+Uses the [Gulp](http://gulpjs.com/) task runner to compile [Sass](http://sass-lang.com/) files, [lint](http://jshint.com/) and concatenate JavaScript and create static HTML from [Handlebars](http://handlebarsjs.com/).
+
+
+## Contents
+1. [Dependencies](#dependencies)
+2. [Installation](#installation)
+3. [Usage](#usage)
+  1. [Tasks](#tasks)
+  2. [Sass](#sass)
+  3. [JavaScript](#javascript)
+  4. [Handlebars](#handlebars)
+  5. [Holder.js](#holderjs)
+  6. [Images](#images)
+4. [Styleguide CSS](#styleguide-css)
+5. [Credits](#credits)
+
+
+## Dependencies
+* [Node.js with npm](https://nodejs.org/)
+* Windows: Use [Node.js 6.x.x](https://nodejs.org/en/download/current/) to avoid paths that exceed 260 characters.
+
+
+## Installation
+1. Get the [latest Front-End Styleguide release](https://github.com/MVSde/styleguide/releases/latest).
+2. Run `npm install` to download Node Modules.
+3. Install global Gulp with `npm install -g gulp`. This step is required to get CLI access to gulp.
+
+
+*To update previously downloaded Node Modules run `npm update`.*
+
+
+## Usage
+
+### Tasks
+These are the main Gulp tasks:
+* Run `gulp` to start the default task. This task watches for file changes and starts Browsersync.  
+All generated files are placed in `dev`.
+* Run `gulp development` to start the default task without file watching and Browsersync.  
+All generated files are placed in `dev`.
+* Run `gulp production` to create prduction ready files. This task minifys CSS and JavaScript files and compresses images.  
+All genereated files will be placed in `dist`.
+
+There are more tasks available for standalone execution:
+* `sass-dev` and `sass-dist` for Sass compilation.
+* `js-dev` and `js-dist` for JavaScript concatenation and linting.
+* `html-dev` and `html-dist` for static HTML file generation.
+* `assets-dev` and `assets-dist` for assets copying and image minification.
+
+*The generated folders `dev` and `dist` are not versioned by Git.*
+
+
+### Sass
+Located in `src/css`.  
+Output to `dev/css` or `dist/css`.
+
+*Sass* is a CSS-Preprocessor supporting variables, nesting and mixins - among many other features. For a complete documentation jump to the [Sass Basics](http://sass-lang.com/guide).
+
+This styleguide splits the CSS into small parts. This ensures a far better organization of style declarations. Each component sits in it's own file and is re-usable across the project. See [Handlebars-Section](#handlebars) for the HTML-side of componentization.
+
+The function `@import` includes the corresponding file in the main Sass file. The final output is one large CSS file to minimize requests. See `src/css/main.scss` for more information.
+
+*The development task generates sourcemaps.*
+
+
+### JavaScript
+Located in `src/js`.  
+Output to `dev/js` or `dist/js`.
+
+JavaScript files are concatenated in the following order: first files from `libraries`, second files from `components`. Within these folders the order is alphabetical.
+
+All 3rd-party stuff should be placed inside the folder `src/js/libraries`. JSHint ignores files in this folder to prevent error spamming. jQuery 2 is included with this styleguide but it can be replaced with another library or removed completely.
+
+*The development task runs JSHint and generates sourcemaps. The production task uglifies the source.*
+
+
+### Handlebars
+Located in `src/html`.<br>
+Output to `dev` or `dist`.
+
+*Handlebars* is an HTML templating engine based on JavaScript. Gulp creates static HTML from Handlebars files.
+
+Hierarchy with subfolders is supported. The output reflects the input file tree.
+
+
+#### Layouts
+Layouts determine the overall structure of the HTML document. They contain the `<head>` area, scripts and styles.
+
+Layouts are located in `src/html/partials/layouts`.
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>{{file.meta.title}}</title>
+  </head>
+  <body>
+    {{{block "content"}}}
+  </body>
+</html>
+```
+
+
+#### Includes
+These files can be included in all other files (even in other includes). The process is comparable to PHP's `include`.
+
+Includes are located in `src/html/partials/includes`. The Handlebars renderer accesses them relative to the `src/html/partials` folder.
+
+The syntax `{{> "includes/example" }}` includes the file `src/html/partials/includes/example.hbs`.
+
+
+#### Pages and Components
+Pages are the actual web pages displayed by the browser.<br>
+Components are reference pages for smaller UI parts. The gulp task `production` will not render them.
+
+The `default` gulp task injects a flyout menu into pages and components for easier navigation during development.
+
+Pages are located in `src/html/pages`, components in `src/html/components`.
+
+The YAML front matter between `---` contains general information like the page title and description. This will be used primarily by layouts. You can add additional information, which can be accessed via `{{file.meta.key}}`. Replace `key` with the name from the front matter.
+
+```html
+---
+title: Example
+description: Only used for components.
+---
+{{#extend "layouts/components"}}
+  {{#content "content"}}
+    <p>This will be injected into the layout.</p>
+  {{/content}}
+{{/extend}}
+```
+
+### Holder.js
+Use [Holder.js](https://github.com/imsky/holder) to include image placeholders.
+
+This basic example generates a grey 300 by 200 pixel image.
+
+```html
+<img src="" data-src="holder.js/300x200" alt="Image Placeholder">
+```
+
+*[More advanced options](https://github.com/imsky/holder#placeholder-options) like color, text and font are available.*
+
+
+### Images
+Located in `src/img`.<br>
+Output to `dev/img` or `dist/img`.
+
+All files and folders placed in `src/img` will be copied to `dev/img` or `dist/img`.
+
+*The production task minifies images with a lossless compressor.*
+
+
+## Styleguide CSS
+The components pages and the development menu use some styling. All styles are located in `src/html/css/sg.scss`.
+
+*The style definitions located in `src/css` use the prefix `sg-` to ensure compatibility with the main stylesheet.*
+
+
+## Credits
+
+[Material icons by Google](https://design.google.com/icons/)
---
+++ gulpfile.js
@@ -0,0 +1,437 @@
+'use strict';
+
+
+
+
+/* NODE MODULES
+ * ========================================================================== */
+
+
+// Base
+var glob         = require('glob');
+var del          = require('del');
+var path         = require('path');
+
+// Gulp
+var gulp         = require('gulp');
+
+// Utilities
+var gutil        = require('gulp-util');
+var rename       = require('gulp-rename');
+var concat       = require('gulp-concat');
+var runSequence  = require('run-sequence');
+
+// CSS/Sass
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+// JavaScript
+var uglify       = require('gulp-uglify');
+var jshint       = require('gulp-jshint');
+var stylish      = require('jshint-stylish');
+
+// Handelbars
+var hb           = require('gulp-hb');
+var frontMatter  = require('gulp-front-matter');
+
+// Images
+var imagemin     = require('gulp-imagemin');
+
+// BrowserSync
+var browserSync  = require('browser-sync');
+
+
+
+
+/* PATHS & CONFIGURATION
+ * ========================================================================== */
+
+
+// Base folders
+var basePaths = {
+  src:  'src/',
+  dev:  'dev/',
+  dist: 'dist/'
+}
+
+
+// Task specific folders
+var paths = {
+  // CSS
+  css: {
+    src:        basePaths.src  + 'css/',
+    dev:        basePaths.dev  + 'css/',
+    dist:       basePaths.dist + 'css/'
+  },
+
+  // JavaScript
+  js: {
+    src:        basePaths.src  + 'js/',
+    dev:        basePaths.dev  + 'js/',
+    dist:       basePaths.dist + 'js/'
+  },
+
+  // HTML
+  html: {
+    src:        basePaths.src  + 'html/',
+    pages:      basePaths.src  + 'html/pages/',
+    components: basePaths.src  + 'html/components/',
+    partials:   basePaths.src  + 'html/partials/**/*.hbs',
+    css:        basePaths.src  + 'html/css/',
+    dev:        basePaths.dev,
+    dist:       basePaths.dist
+  },
+
+  // Images
+  img: {
+    src:        basePaths.src  + 'img/',
+    dev:        basePaths.dev  + 'img/',
+    dist:       basePaths.dist + 'img/'
+  }
+}
+
+
+// Config
+var config = {
+  // CSS
+  css: {
+    dev: {
+      outputStyle: 'expanded'
+    },
+    dist: {
+      outputStyle: 'compressed'
+    },
+    autoprefixer: {
+      browsers: ['last 2 versions']
+    }
+  },
+
+  // HTML
+  html: {
+    hb: {
+      bustCache: true
+    },
+    browserSync: {
+      server: {
+        baseDir: paths.html.dev
+      },
+      logPrefix: 'BrowserSync',
+      scrollElements: ['*'],
+      reloadDelay: 200
+    }
+  },
+
+  // Images
+  img: {
+    imagemin: {
+      progressive: true,
+      interlaced: true,
+      multipass: true
+    }
+  }
+}
+
+
+
+
+/* CLEAN
+ * Delete directories
+ * ========================================================================== */
+
+
+// Clean Development
+gulp.task('clean-dev', function() {
+  return del(basePaths.dev);
+});
+
+
+// Clean Distribution
+gulp.task('clean-dist', function() {
+  return del(basePaths.dist);
+});
+
+
+// Clean Dev HTML
+gulp.task('clean-html-dev', function() {
+  return del(paths.html.dev + '**/*.html');
+});
+
+
+// Clean Dev Images
+gulp.task('clean-img-dev', function() {
+  return del(paths.img.dev + '**');
+});
+
+
+
+
+/* SASS
+ * Compile Sass code into CSS
+ * ========================================================================== */
+
+
+// CSS Development
+gulp.task('css-dev', function() {
+  return gulp.src(paths.css.src + '**/*.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass(config.css.dev).on('error', sass.logError))
+      .pipe(autoprefixer(config.css.autoprefixer))
+      .pipe(rename('styles.css'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.css.dev))
+    .pipe(browserSync.stream({match: '**/*.css'}));
+});
+
+
+// CSS Watch
+gulp.task('css-watch', ['css-dev']);
+
+
+// CSS Styleguide
+gulp.task('css-sg', function() {
+  return gulp.src(paths.html.css + 'sg.scss')
+    .pipe(sass(config.css.dev).on('error', sass.logError))
+    .pipe(autoprefixer(config.css.autoprefixer))
+    .pipe(gulp.dest(paths.css.dev));
+});
+
+
+// CSS Distribution
+gulp.task('css-dist', ['clean-dist'], function() {
+  return gulp.src(paths.css.src + '**/*.scss')
+    .pipe(sass(config.css.dist).on('error', sass.logError))
+    .pipe(autoprefixer(config.css.autoprefixer))
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest(paths.css.dist));
+});
+
+
+
+
+/* JAVASCRIPT
+ * Lint javscript
+ * ========================================================================== */
+
+
+// JavaScript Hint
+gulp.task('js-hint', function() {
+  return gulp.src([paths.js.src + '**/*.js', '!' + paths.js.src + 'libraries/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+
+// JavaScript Concatenation
+gulp.task('js-concat', function() {
+  return gulp.src([paths.js.src + 'libraries/**/*.js', paths.js.src + 'components/**/*.js'])
+    .pipe(sourcemaps.init())
+      .pipe(concat('scripts.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.js.dev))
+    .pipe(browserSync.stream({match: '**/*.js'}));
+});
+
+
+// JavaScript Development
+gulp.task('js-dev', ['js-hint', 'js-concat']);
+
+
+// JavaScript Watch
+gulp.task('js-watch', ['js-dev']);
+
+
+// JavaScript Production
+gulp.task('js-dist', ['clean-dist'], function() {
+  return gulp.src([paths.js.src + 'libraries/**/*.js', paths.js.src + 'components/**/*.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.js.dist));
+});
+
+
+
+
+/* HTML
+ * Pre-compile Handlebars files into HTML
+ * ========================================================================== */
+
+
+// Search string in array and replace
+var replaceInArray = function(array, searchFor, replaceWith) {
+  for (var i = 0; i < array.length; i++) {
+    array[i] = array[i].replace(searchFor, replaceWith);
+  }
+};
+
+
+// Handle Handlebars error
+var onErrorHandler = function(error) {
+  console.log(gutil.colors.red('Handlebars error'));
+  console.log('File: ' + gutil.colors.underline(error.fileName));
+  console.log('Message: ' + error.message + '\n');
+};
+
+
+// Handlebars Function
+var compileHandlebars = function(source, destination, nav) {
+  var pageGlob = glob.sync(paths.html.pages      + '**/*.hbs');
+  var compGlob = glob.sync(paths.html.components + '**/*.hbs');
+
+  replaceInArray(pageGlob, paths.html.pages, '');
+  replaceInArray(compGlob, paths.html.components, '');
+
+  var pageList = [];
+  var compList = [];
+
+  for (var i = 0, item; item = pageGlob[i++];) {
+    pageList.push({
+      title: item.replace('.hbs', ''),
+      href:  item.replace('.hbs', '.html')
+    });
+  }
+
+  for (var i = 0, item; item = compGlob[i++];) {
+    compList.push({
+      title: item.replace('.hbs', ''),
+      href:  'components/' + item.replace('.hbs', '.html')
+    });
+  }
+
+  var hbStream = hb(config.html.hb)
+    .partials(paths.html.partials)
+    .helpers(require('handlebars-layouts'))
+    .helpers({
+      rel: function(options) {
+        var currentPath = path.dirname(options.data.file.path);
+        var sourcePath  = path.resolve(source);
+
+        var additionalPath = '';
+
+        if (source === paths.html.components) {
+          additionalPath = '../'
+        }
+
+        if (currentPath === sourcePath) {
+          return additionalPath;
+        }
+
+        return additionalPath + path.relative(currentPath, sourcePath) + '/';
+      }
+    })
+    .data({
+      displayNav: nav,
+      navItems: {
+        pages: pageList,
+        components: compList
+      }
+    });
+
+  return gulp.src(source + '**/*.hbs')
+    .pipe(frontMatter({property: 'meta'}))
+    .pipe(hbStream.on('error', onErrorHandler))
+    .pipe(rename({extname: '.html'}))
+    .pipe(gulp.dest(destination));
+};
+
+
+// HTML Development
+gulp.task('html-dev', ['clean-html-dev'], function() {
+  compileHandlebars(paths.html.pages, paths.html.dev, true);
+  compileHandlebars(paths.html.components, paths.html.dev + 'components', true);
+});
+
+
+// HTML Watch
+gulp.task('html-watch', ['html-dev'], browserSync.reload);
+
+
+// HTML Production
+gulp.task('html-dist', ['clean-dist'], function() {
+  compileHandlebars(paths.html.pages, paths.html.dist, false);
+});
+
+
+
+
+/* Images
+ * Copy images and compress production files
+ * ========================================================================== */
+
+
+// Images Development
+gulp.task('img-dev', ['clean-img-dev'], function() {
+  return gulp.src(paths.img.src + '**')
+    .pipe(gulp.dest(paths.img.dev));
+});
+
+
+// Images Watch
+gulp.task('img-watch', ['img-dev'], browserSync.reload);
+
+
+// Images Distribution
+gulp.task('img-dist', ['clean-dist'], function() {
+  return gulp.src(paths.img.src + '**')
+    .pipe(imagemin(config.img.imagemin))
+    .pipe(gulp.dest(paths.img.dist));
+});
+
+
+
+
+/* Development
+ * Generate development files.
+ * ========================================================================== */
+
+
+gulp.task('development', ['css-dev', 'js-dev', 'html-dev', 'img-dev']);
+
+
+
+
+/* Production
+ * Generate production ready files.
+ * ========================================================================== */
+
+
+gulp.task('production', ['css-dist', 'js-dist', 'html-dist', 'img-dist']);
+
+
+
+
+/* DEFAULT
+ * Run all dev tasks and watch for changes.
+ * ========================================================================== */
+
+
+gulp.task('browsersync', function() {
+  browserSync(config.html.browserSync);
+});
+
+
+gulp.task('default', ['clean-dev'], function() {
+  var onChangeMessage = function(event) {
+    console.log('\n');
+    gutil.log(gutil.colors.blue(event.path) + ' ' + event.type);
+  }
+
+  // Run initial task queue
+  runSequence(['css-dev', 'css-sg', 'js-dev', 'html-dev', 'img-dev'], 'browsersync');
+
+  // Watch CSS
+  var watchCSS = gulp.watch(paths.css.src + '**/*.scss', ['css-watch']);
+  watchCSS.on('change', onChangeMessage);
+
+  // Watch JavaScript
+  var watchJS = gulp.watch(paths.js.src + '**/*.js', ['js-watch']);
+  watchJS.on('change', onChangeMessage);
+
+  // Watch HTML
+  var watchHTML = gulp.watch(paths.html.src + '**/*.hbs', ['html-watch']);
+  watchHTML.on('change', onChangeMessage);
+
+  // Watch Images
+  var watchImg = gulp.watch(paths.img.src + '**/*', ['img-watch']);
+  watchImg.on('change', onChangeMessage);
+});
