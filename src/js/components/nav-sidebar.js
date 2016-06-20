e8da9423d00d53f465f4a20189c6ef4d1c3cc58e
Fynn Becker <info@fynn-becker.de>
2016-06-20 09:42:38 +0200

    Add animation for fly-out sidebar navigation

--- src/js/components/nav-sidebar.js
+++ src/js/components/nav-sidebar.js
@@ -6,19 +6,20 @@
 ;(function() {
   'use strict';
 
-  let menuButton   = document.getElementsByClassName('js-sidebar-button');
-  let sidebarCover = document.getElementsByClassName('js-sidebar-cover')[0];
-  let sidebar      = document.getElementsByClassName('js-sidebar')[0];
+  let button  = document.getElementsByClassName('js-sidebar-button');
+  let cover   = document.getElementsByClassName('js-sidebar-cover')[0];
+  let sidebar = document.getElementsByClassName('js-sidebar')[0];
 
   function toggleSidebar() {
     sidebar.classList.toggle('is-visible');
+    cover.classList.toggle('is-visible');
   }
 
-  for (let i = 0; i < menuButton.length; i++) {
-    menuButton[i].addEventListener('click', toggleSidebar);
+  for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', toggleSidebar);
   }
 
-  if (sidebarCover) {
-    sidebarCover.addEventListener('click', toggleSidebar);
+  if (cover) {
+    cover.addEventListener('click', toggleSidebar);
   }
 })();
