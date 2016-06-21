aa3e02b8d1e39e46edb3db26deb85204578f71f0
Fynn Becker <info@fynn-becker.de>
2016-06-21 15:48:21 +0200

    Experiment with basic JavaScript error throwing

--- src/js/functions/toggle.js
+++ src/js/functions/toggle.js
@@ -9,6 +9,18 @@
 // Toggle Buttons
 
 function toggle(element, buttonClass, panelClass) {
+  if (!element) {
+    throw new Error('Element missing.')
+  }
+
+  if (!buttonClass) {
+    throw new Error('Class for activated button missing.');
+  }
+
+  if (!panelClass) {
+    throw new Error('Class for activated panel missing.');
+  }
+
   element.getElementsByClassName('js-toggle-button')[0].addEventListener('click', function() {
     this.classList.toggle(buttonClass);
     element.getElementsByClassName('js-toggle-panel')[0].classList.toggle(panelClass);
@@ -16,9 +28,22 @@ function toggle(element, buttonClass, panelClass) {
 }
 
 
+
 // Toggle Form Autocomplete
 
 function toggleInput(element, buttonClass, panelClass) {
+  if (!element) {
+    throw new Error('Element missing.')
+  }
+
+  if (!buttonClass) {
+    throw new Error('Class for activated button missing.');
+  }
+
+  if (!panelClass) {
+    throw new Error('Class for activated panel missing.');
+  }
+
   element.getElementsByClassName('js-form-autocomplete-input')[0].addEventListener('focus', function() {
     this.classList.add(buttonClass);
     element.getElementsByClassName('js-form-autocomplete-panel')[0].classList.add(panelClass);
